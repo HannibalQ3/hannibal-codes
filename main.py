d7de434d3690cfdb31f8a38a918b4e1ceaf829f9
HannibalQ3 <134856307+HannibalQ3@users.noreply.github.com>
2023-05-28 11:12:25 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+basic.show_icon(IconNames.NO)
 xgo.init_xgo_serial(SerialPin.P2, SerialPin.P1)
-basic.show_icon(IconNames.HEART)
 
 def on_forever():
-    pass
+    basic.show_icon(IconNames.HEART)
+    basic.show_icon(IconNames.SMALL_HEART, 450)
+    basic.clear_screen()
+    basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 basic.showIcon(IconNames.No)
 xgo.init_xgo_serial(SerialPin.P2, SerialPin.P1)
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart, 450)
+    basic.clearScreen()
+    basic.pause(100)
 })
